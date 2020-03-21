19cc37ac8cebac3771aaf07288e270ae5923106b
jrandj <james.randjelovic@gmail.com>
2020-03-21 17:57:28 +1100

    Fix naming

--- example/src/main/java/function/example/App.java
+++ example/src/main/java/function/example/App.java
@@ -1,18 +1,16 @@
-package storedprocedure.example;
+package function.example;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 /**
- * An example for calling an ODB function.
+ * An example class calling an ODB function.
  *
  */
 public class App {
